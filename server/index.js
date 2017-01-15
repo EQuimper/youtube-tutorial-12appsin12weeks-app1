6382ad1ed0c886f43441d1998c22dd5b8f0e1c90
EQuimper <quimperemanuel@gmail.com>
2017-01-15 15:04:10 -0500

    End ep02

--- server/index.js
+++ server/index.js
@@ -1,15 +1,26 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 
 const PORT = process.env.PORT || 3000;
+
+/*
+* DATABASE
+*/
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/shorturltuto');
+mongoose.connection
+  .once('open', () => console.log('MONGODB connected'))
+  .on('error', err => console.error(err));
+
 
 app.get('/', (req, res) => {
   res.send('Hello');
 });
 
 app.listen(PORT, err => {
-  if (err) { return console.error(err) }
+  if (err) { return console.error(err); }
 
-  console.log(`App listen to port ${PORT}`);
+  console.log(`App listen to ${PORT}`);
 });
